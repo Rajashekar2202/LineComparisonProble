d5b2364e6deaabfac57e6a7cea2cca408d104fde
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-25 19:24:31 +0530

    added Two line Comaprison Program

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            Length line = new Length();
-            line.Distance();
+            //Length line = new Length();
+            //line.Distance();
+            //CheckEquality line = new CheckEquality();
+            //line.LenghthEqualityCheck();
+            TwoLineComparision line = new TwoLineComparision();
+            line.Check();
 
         }
     }
